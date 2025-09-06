691482d33783a1580e335790e50cf0c683953ad1
VinayPratap07 <vpstanwar07@gmail.com>
2025-09-06 11:40:40 +0530

    Added fonts and updated some styles

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="container max-w-screen  ">
-      <header className="flex justify-evenly items-center h-20 w-full shrink-0 md:px-6">
-        <h1 className="font-mono text-5xl text-[#111827] w-1/4 pl-20 font-bold">
+    <div className="container max-w-screen shadow-[0_2px_4px_rgba(0,0,0,0.1)]">
+      <header className="flex justify-between items-center h-20 w-full shrink-0 md:px-6">
+        <h1 className="font-Poppins text-4xl text-[#111827] w-1/4 pl-20">
           Velora
         </h1>
 
@@ -14,7 +14,8 @@ function Header() {
           </button>
           <input
             type="text"
-            className="border-b w-17/20 mx-6 p-1 text-xl hover:border-b-[#3B82F6] focus:border-b-[#3B82F6] focus:outline-none"
+            className="border-b w-17/20 mx-6 p-1 text-l hover:border-b-[#3B82F6] focus:border-b-[#3B82F6] focus:outline-none"
+            placeholder="Search for products...."
           />
         </form>
 
@@ -22,10 +23,10 @@ function Header() {
           <NavLink to="/" className="headerNavLink textFont">
             Home
           </NavLink>
-          <NavLink to="/cart" className="headerNavLink">
+          <NavLink to="/cart" className="headerNavLink textFont">
             Cart
           </NavLink>
-          <NavLink to="/wishlist" className="headerNavLink">
+          <NavLink to="/wishlist" className="headerNavLink textFont">
             Wishlist
           </NavLink>
           <button className="p-1 border border-[#6B7280] ">Light</button>
